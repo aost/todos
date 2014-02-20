f48477b48f7c5ede40fb6c0b6b85b8d601892b39
Ryan Stout <ryanstout@gmail.com>
2014-02-20 15:19:49 -0700

    WIP

--- app/home/controllers/index_controller.rb
+++ app/home/controllers/index_controller.rb
@@ -1,20 +1,24 @@
 class IndexController < ModelController
   # Change to :store and run mongod to store
-  model :page
+  # model self.local_store
 
   def initialize
     super
-    
-    self._current_todo = _todos[params._index.to_i]
-    
+    @model = $page.local_store
+
+    page._current_todo = _todos[params._index.to_i]
+
     # Add controller setup code here
     page._all_completed.on('changed') do
       set_all(page._all_completed.cur)
     end
-    
-    page._todos << {_label: 'Yep'}
+
+    # page._todos << {_label: 'Yep'}
+
+    params._index.on('changed') { puts "INDEX CHANGED" }
+    page._current_tood.on('changed') { puts "TCD changed" }
   end
-  
+
   def add_todo
     puts "ADD TODO"
     flash._notices << "todo '#{page._new_todos._label.to_s}' added"
@@ -22,15 +26,15 @@ class IndexController < ModelController
     self._todos << page._new_todos.to_h.cur
     page._new_todos = {}
   end
-  
+
   def remove_todo(index)
     self._todos.delete_at(index)
   end
-  
+
   def set_index(index)
     params._index = index
   end
-  
+
   def set_all(completed)
     _todos.each do |todo|
       todo._completed = completed
